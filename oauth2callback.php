b822b0567893802af5a3a217665ed5997be2f420
nayeem1910 <mahamudulhassannayeem1910@gmail.com>
2025-06-25 13:52:28 +0600

    Update oauth2callback.php

--- oauth2callback.php
+++ oauth2callback.php
@@ -6,7 +6,7 @@ session_start();
 $client = new Google_Client();
 $client->setClientId('982444537092-ku2dcpeolqd15oeqrcura8s86c8pshls.apps.googleusercontent.com');
 $client->setClientSecret('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');
-$client->setRedirectUri('https://yourdomain.com/oauth2callback.php');
+$client->setRedirectUri('https://new-kb9a.onrender.com/oauth2callback.php');
 $client->addScope('https://www.googleapis.com/auth/youtube.upload');
 
 if (isset($_GET['code'])) {
