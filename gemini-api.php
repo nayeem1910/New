81a6049336a5f5609b7c65758764c14ca0f26209
nayeem1910 <mahamudulhassannayeem1910@gmail.com>
2025-06-26 00:15:23 +0600

    Create gemini-api.php

---
+++ gemini-api.php
@@ -0,0 +1,26 @@
+<?php
+$apiKey = ""; // Keep this secret
+$inputText = $_POST['text'] ?? '';
+
+$data = [
+  "contents" => [
+    [
+      "parts" => [
+        [ "text" => $inputText ]
+      ]
+    ]
+  ]
+];
+
+$options = [
+  'http' => [
+    'method'  => 'POST',
+    'header'  => "Content-Type: application/json\r\n",
+    'content' => json_encode($data)
+  ]
+];
+
+$context = stream_context_create($options);
+$response = file_get_contents("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$apiKey", false, $context);
+echo $response;
+?>
